bde9332a654bbb6e6274dc3310a83fa3d1a6174e
Nikita Lipatov <6stringsohei@gmail.com>
2022-11-14 23:28:43 +0300

    UserDTO and UserClaimsDTO are now in LimpShared submodule

--- AuthAPI/DTOs/Claims/UserClaimsDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿#nullable disable
-namespace AuthAPI.DTOs.Claims
-{
-    public class UserClaimsDTO
-    {
-        public string Name { get; set; }
-        public string Value { get; set; }
-    }
-}
