d740c94ae58b1ccf731a219a7170cca8b6ae946b
Nikita Lipatov <6stringsohei@gmail.com>
2022-11-24 01:48:42 +0300

    UsersController now has method that returns user's username from user's access-token

--- AuthAPI/Controllers/UsersController.cs
+++ AuthAPI/Controllers/UsersController.cs
@@ -1,7 +1,10 @@
 ﻿using AuthAPI.DTOs.User;
 using AuthAPI.Mapping;
+using AuthAPI.Services.JWT;
 using AuthAPI.Services.UserProvider;
 using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace AuthAPI.Controllers
 {
@@ -10,9 +13,11 @@ namespace AuthAPI.Controllers
     public class UsersController : Controller
     {
         private readonly IUserProvider _userProvider;
-        public UsersController(IUserProvider userProvider)
+        private readonly IJwtService _jwtService;
+        public UsersController(IUserProvider userProvider, IJwtService jwtService)
         {
             _userProvider = userProvider;
+            _jwtService = jwtService;
         }
         [HttpGet("UsersOnline")]
         public async Task<ActionResult<List<UserDTO>>> GetUsersOnline()
@@ -24,6 +29,17 @@ namespace AuthAPI.Controllers
                 .ToList();
 
             return Ok(dtos);
+        }
+
+        [HttpGet("GetUserName")]
+        public async Task<ActionResult<string>> GetUserName(string accessToken)
+        {
+            bool isTokenValid = _jwtService.ValidateAccessToken(accessToken);
+
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            JwtSecurityToken securityToken = tokenHandler.ReadToken(accessToken) as JwtSecurityToken;
+
+            return securityToken!.Claims.FirstOrDefault(x => x.Type == "unique_name")?.Value ?? "Anonymous";
         }
     }
 }
