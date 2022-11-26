74b2ef6f5d69a76742f7a04b249025fabe4d0dd7
Nikita Lipatov <6stringsohei@gmail.com>
2022-11-26 21:45:56 +0300

    Added HealthController.

---
+++ AuthAPI/Controllers/HealthController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        private string[] oKResponses = new string[5]
+        {
+            "Yes!",
+            "Yes I am!",
+            "I wouldn't call it 'living' but it'll do",
+            "Physically, yes",
+            "Yep"
+        };
+
+        [HttpGet]
+        public async Task<ActionResult<string>> IsServiceAlive()
+        {
+            Random random = new Random();
+
+            string response = oKResponses[random.Next(0, 4)];
+
+            return Ok(response);
+        }
+    }
+}
