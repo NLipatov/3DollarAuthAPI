7a9ef9ee0e538cf6db734f871a8bd7d0d46e7952
Nikita Lipatov <6stringsohei@gmail.com>
2023-08-26 14:30:19 +0300

    RefreshTokenHistory.cs - new property added.

--- AuthAPI/DB/Models/RefreshTokenHistory.cs
+++ AuthAPI/DB/Models/RefreshTokenHistory.cs
@@ -6,5 +6,6 @@ public record RefreshTokenHistory
     public Guid Id { get; set; } = Guid.NewGuid();
     public User User { get; set; }
     public string UserAgent { get; set; }
+    public Guid UserAgentId { get; set; }
     public DateTime DateTime { get; set; } = DateTime.UtcNow;
 }
