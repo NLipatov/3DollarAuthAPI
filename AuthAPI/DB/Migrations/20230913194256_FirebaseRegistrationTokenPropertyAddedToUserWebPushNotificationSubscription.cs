cb5cdf39eeb2f34b062300aa22aff0c1b148f69f
Nikita Lipatov <6stringsohei@gmail.com>
2023-09-13 23:55:42 +0300

    added migrations

---
+++ AuthAPI/DB/Migrations/20230913194256_FirebaseRegistrationTokenPropertyAddedToUserWebPushNotificationSubscription.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirebaseRegistrationTokenPropertyAddedToUserWebPushNotificationSubscription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirebaseRegistrationToken",
+                table: "WebPushNotificationSubscriptions",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirebaseRegistrationToken",
+                table: "WebPushNotificationSubscriptions");
+        }
+    }
+}
