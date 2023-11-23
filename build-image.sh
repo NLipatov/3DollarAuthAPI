6e9456949ada4408494f3e02aad1ba8c5385c6d0
Nikita Lipatov <6stringsohei@gmail.com>
2023-11-23 20:05:32 +0400

    minor fix for build-image script

--- build-image.sh
+++ build-image.sh
@@ -1,4 +1,4 @@
-if [ $# -ne 1 ]; then
+if [ $# -ne 2 ]; then
     echo "Usage: $0 <folder that contains appsettings.json and FCMConfiguration.json> $1 <folder that contains dev certificate>"
     exit 1
 fi
